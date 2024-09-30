f0b7d7c998ece4c3672b552c818fb06ad741e994
R.Girinath <125426416+girinath18@users.noreply.github.com>
2024-09-30 15:40:01 +0530

    bot_world folders

---
+++ bot_world/chat_bot/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+from . import views
+from django.contrib.auth.views import LogoutView
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('signup/', views.signup, name='signup'),
+    path('login/', views.login_view, name='login'),
+    path('profile/', views.profile_view, name='profile'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
+    path('chat/', views.chat_view, name='chat_view'),
+    path('upload/', views.upload_file, name='upload_file'),
+]
+
+# Serve media files during development
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
